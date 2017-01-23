453926c5e157ed9893f05617890d3f22b6e648dc
DavidM-WA <david.melia@web-atrio.com>
2017-01-23 16:49:11 +0100

    Configuring angular

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# SpringBoot-Angular2
+# Spring-Boot / Angular2 Quickstart
+
+## Angular 2
+
+### Commands
+
+- Run `npm i` in root directory to install modules
+- Run `ng serve --proxy-config proxy.conf.json` to launch the angular project with Live Reload
+
+## Spring boot
+
+### Commands
+
+- Run `./gradlew build` to compile
+- Change directory with `cd build/libs/`
+- Run `java -jar project-0.0.1-SNAPSHOT.jar` to launch the server
---
+++ src/main/java/com/web/atrio/controllers/DefaultController.java
@@ -0,0 +1,16 @@
+package com.web.atrio.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+	
+	@RequestMapping("/hello")
+	public ResponseEntity sayHello(@RequestParam(value="name", defaultValue="bob") String name){
+		return new ResponseEntity<String>("Hello, " + name, HttpStatus.OK);
+	}
+  }
