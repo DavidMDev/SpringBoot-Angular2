b873531a0471660eefecc718caa345f66b3d1442
DavidM-WA <david.melia@web-atrio.com>
2017-02-11 00:00:34 +0100

    Removed useless import

--- src/main/java/com/web/atrio/configuration/CSRFCustomRepository.java
+++ src/main/java/com/web/atrio/configuration/CSRFCustomRepository.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
