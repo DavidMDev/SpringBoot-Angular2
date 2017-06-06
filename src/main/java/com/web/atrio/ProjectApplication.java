2d4777e4b3abece27696dceb8b7b2630d3a6ceae
DavidM-WA <david.melia@web-atrio.com>
2017-06-06 10:19:46 +0200

    Changed the application properties to just create instead of create-drop for the database generation, removed generation of a test user, updated readme

--- src/main/java/com/web/atrio/ProjectApplication.java
+++ src/main/java/com/web/atrio/ProjectApplication.java
@@ -1,16 +1,7 @@
 package com.web.atrio;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.web.atrio.users.models.Account;
-import com.web.atrio.users.models.Address;
-import com.web.atrio.users.models.Telephone;
-import com.web.atrio.users.repositories.AccountRepository;
-import com.web.atrio.users.repositories.AddressRepository;
-import com.web.atrio.users.repositories.TelephoneRepository;
 
 @SpringBootApplication
 public class ProjectApplication {
@@ -19,39 +10,4 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(final AccountRepository accountRepository, final AddressRepository addressRepository, final TelephoneRepository telephoneRepository) {
-
-		return new CommandLineRunner() {
-
-			@Override
-			public void run(String... arg0) throws Exception {
-				Account user = new Account();
-				user.setEmail("dmelia@dmelia.fr");
-				user.setFirstName("David");
-				user.setLastName("Melia");
-				user.setPassword("test");
-				user.setUserName("dmelia");
-				user.addRole("USER");
-				user.addRole("ADMIN");
-				accountRepository.save(user);
-				
-				Address address = new Address();
-				address.setStreetName("Rue du Luan");
-				address.setCity("Balma");
-				address.setHouseNumber("4");
-				address.setPostcode("31130");
-				address.setUser(user);
-				addressRepository.save(address);
-				
-				Telephone telephone = new Telephone();
-				telephone.setNumber("0689276241");
-				telephone.setType("Mobile");
-				telephone.setUser(user);
-				telephoneRepository.save(telephone);
-			}
-
-		};
-
-	}
 }
